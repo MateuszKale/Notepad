2905b223ad3ec891ae837c78d21c9f491d5f39e4
MateuszKale <mateuszkaleta284@gmail.com>
2024-12-11 22:30:44 +0100

    Adding debugger and html templates

--- index.php
+++ index.php
@@ -1,28 +1,67 @@
 <?php
 
-//echo "Witam";
+//declare(strict_types=1);
+
+namespace App;
+
+require_once("src/Utils/debug.php");
 
 $test = 'test';
 
-
-function dump($data)
-{
-    echo '<div 
-        style="
-            display: inline-block;
-            padding: 0 10px;
-            border: 1px solid gray;
-            background: lightgray
-        "
-    >
-    <pre>';
-    print_r($data);
-    echo '</pre>
-    </div>
-    </br>';
+if (!empty($_GET['action'])){
+    $action = $_GET['action'];
+}else{
+    $action = null;
 }
- 
-dump($test);
-dump(3333);
-dump('abc');
+
+
+
+
+
+?>
+
+<html>
+
+
+<head>
+</head>
+        
+<body>
+    <div class="header">
+        <h1>Moje notatki</h1>
+    </div>
+        
+    <div class="conainter">
+        <div class="menu">
+            <ul>
+                <li>
+                    <a href="/">Lista notatek</a>
+                </li>
+                <li>
+                    <a href="/?action=create">Nowa notatka</a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <?php
+                if($action=== 'create') :
+                    echo 'nowa notatka';
+                else:
+                    echo 'lista notatek';
+                endif;
+            ?>
+        </div>
+        
+    </div>
+
+    <div class="footer">
+
+    </div>
+
+</body>
+
+
+
+</html>
 
