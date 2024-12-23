65d7f75e6c02df31dc8fdf60eccc59aa30d72a06
MateuszKale <mateuszkaleta284@gmail.com>
2024-12-23 17:25:43 +0100

    create db info

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+  'db' =>[
+    'host' => 'localhost',   // gdyby to nie zadzialalo to uzyc notepad.localhost
+    'database' => 'notes',
+    'user' => 'user_notes',
+    'password' => 'jeG9Or6mM_B41S6J'
+  ]    
+];
