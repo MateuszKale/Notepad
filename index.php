61f2b77b8c1c9ecb7565871e6ae59bb14ca8dd09
MateuszKale <mateuszkaleta284@gmail.com>
2025-01-06 20:40:05 +0100

    Add trycatch exceptions

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Exception\AppException;
+use Throwable;
 
 require_once("src/Utils/debug.php");
 require_once("src/Controller.php");
@@ -15,10 +17,19 @@ $request = [
   'post' => $_POST
 ];
 
-// $controller = new Controller($request);
-// $controller->run();
+
+try {
+  // $controller = new Controller($request);
+  // $controller->run();
+
+  Controller::initConfiguration($configuration);
+  (new Controller($request))->run();
+} catch(AppException $e) {
+} catch (Throwable $e){
+  echo "<h1>Wystąpił błąd w aplikacji</h1>";
+}
 
 
-Controller::initConfiguration($configuration);
-(new Controller($request))->run();
+
+
 
