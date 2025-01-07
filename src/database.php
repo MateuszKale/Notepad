b9bcb10dc0d8839437848446889678211fc416ae
MateuszKale <mateuszkaleta284@gmail.com>
2025-01-07 10:19:12 +0100

    Minior upgrades

--- src/database.php
+++ src/database.php
@@ -4,21 +4,27 @@ declare(strict_types=1);
 
 namespace App;
 
+require_once('Exceptions/AppException.php');
+
+use App\Exception\StorageException;
 use PDO;
+use Throwable;
 
 class Database
 {
   public function __construct(array $config)
   {
-    //dump($config);
+    try{
+      $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
     
-    $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
-    
-    $connection = new PDO(
-      $dsn,
-      $config['user'],
-      $config['password'],
+      $connection = new PDO(
+        $dsn,
+        $config['user'],
+        $config['password'],
     );
+    } catch(Throwable $e){
+      throw new StorageException('Connection error');
+      exit('e');
+    }
   }
-
 }
