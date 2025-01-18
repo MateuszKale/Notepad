0338ef6ae807fdb46421dd7af647818238d42883
MateuszKale <mateuszkaleta284@gmail.com>
2025-01-18 17:05:11 +0100

    s

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
   'db' =>[
-     'host' => 'localhost',   // gdyby to nie zadzialalo to uzyc 
+    'host' => 'localhost',   // gdyby to nie zadzialalo to uzyc 
     'database' => 'notes',
     'user' => 'user_notes',
     'password' => 'jeG9Or6mM_B41S6J'
